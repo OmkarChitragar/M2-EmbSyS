45b495d6f1902efbded59698049ade07763ec209
OmkarChitragar <omkarchitragar@gmail.com>
2022-03-07 01:46:46 +0530

    commit

--- Project/3_Implementation/Project_main.c
+++ Project/3_Implementation/Project_main.c
@@ -1,3 +1,13 @@
+/**
+ * @file Project_main.c
+ * @author omkar chitragar 
+ * @brief 
+ * @version 0.2
+ * @date 2022-03-07
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <avr/io.h>
 #include"Led.h"
 #include"Lcd.h"
